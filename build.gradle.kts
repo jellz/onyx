b6336bbcf7bd53461dd6f623f45246738674f226
Daniel <danielgulic@gmail.com>
2021-10-17 22:06:49 +1100

    Configure Shadow compilation

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,9 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
 plugins {
     kotlin("jvm") version "1.5.10"
+    id("com.github.johnrengelman.shadow") version "6.1.0"
     application
 }
 
@@ -12,12 +14,26 @@ repositories {
     mavenCentral()
 }
 
-
+application {
+    mainClass.set("com.danielgulic.onyx.MainKt")
+    mainClassName = "com.danielgulic.onyx.MainKt"
+}
 
 tasks.withType<KotlinCompile>() {
     kotlinOptions.jvmTarget = "1.8"
 }
 
-application {
-    mainClass.set("MainKt")
+tasks {
+    named<ShadowJar>("shadowJar") {
+        mergeServiceFiles()
+        manifest {
+            attributes(mapOf("Main-Class" to "com.danielgulic.onyx.MainKt"))
+        }
+    }
+}
+
+tasks {
+    build {
+        dependsOn(shadowJar)
+    }
 }
---
+++ src/main/kotlin/com/danielgulic/onyx/Main.kt
@@ -0,0 +1,5 @@
+package com.danielgulic.onyx
+
+fun main(args: Array<String>) {
+    println("Hello World! args: ${args.joinToString()}")
+}
